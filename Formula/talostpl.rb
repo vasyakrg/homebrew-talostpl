eed3a7831dc074f3e627a0e20ff6d6235fbf030a
Vassiliy Yegorov <va@egorovanet.ru>
2025-06-26 09:06:10 +0700

    Добавлено условие для поддержки разных архитектур (arm64 и amd64) в формуле Talostpl, обновлены URL и sha256 для каждой архитектуры.

--- Formula/talostpl.rb
+++ Formula/talostpl.rb
@@ -1,12 +1,22 @@
 class Talostpl < Formula
   desc "Interactive and non-interactive Talos K8s config generator"
   homepage "https://github.com/vasyakrg/talostpl"
-  url "https://github.com/vasyakrg/talostpl/releases/download/v1.0.0/talostpl-darwin-arm64"
+
+  on_arm do
+    url "https://github.com/vasyakrg/talostpl/releases/download/v1.0.0/talostpl-darwin-arm64"
+    sha256 "9843d546bd541b9bf58e2e1c3c85aa8ff0b2f3705630b84b9123be55a99d5202"
+  end
+
+  on_intel do
+    url "https://github.com/vasyakrg/talostpl/releases/download/v1.0.0/talostpl-darwin-amd64"
+    sha256 "<PUT_SHA256_FOR_AMD64_HERE>"
+  end
+
   version "1.0.0"
-  sha256 "9843d546bd541b9bf58e2e1c3c85aa8ff0b2f3705630b84b9123be55a99d5202"
 
   def install
-    bin.install "talostpl-darwin-arm64" => "talostpl"
+    arch = Hardware::CPU.arm? ? "arm64" : "amd64"
+    bin.install "talostpl-darwin-#{arch}" => "talostpl"
   end
 
   test do
