c3bffa1d48010caf22f4767dc0e9e9ec2168bc11
Vassiliy Yegorov <va@egorovanet.ru>
2025-06-26 12:54:49 +0700

    Обновлен URL для версии arm64 в формуле Talostpl.

--- Formula/talostpl.rb
+++ Formula/talostpl.rb
@@ -2,7 +2,7 @@ class Talostpl < Formula
   desc "Interactive and non-interactive Talos K8s config generator"
   homepage "https://github.com/vasyakrg/talostpl"
   version "1.0.4"
-  url "https://github.com/vasyakrg/talostpl/releases/download/v1.0.4/talostpl-darwin-amd64"
+  url "https://github.com/vasyakrg/talostpl/releases/download/v1.0.4/talostpl-darwin-arm64"
   sha256 "87f30b1deba45357bc9828bdd02e2e9ea36ebf9b6f2f74a9e7ecaaab9d719df8"
 
   def install
