cc1957543f0e1f293d33826eafcc5432de8a5b4e
Vassiliy Yegorov <va@egorovanet.ru>
2025-06-26 09:24:02 +0700

    Удалены условия для поддержки разных архитектур в формуле Talostpl, установка теперь осуществляется только для arm64.

--- Formula/talostpl.rb
+++ Formula/talostpl.rb
@@ -2,20 +2,11 @@ class Talostpl < Formula
   desc "Interactive and non-interactive Talos K8s config generator"
   homepage "https://github.com/vasyakrg/talostpl"
   version "1.0.0"
+  url "https://github.com/vasyakrg/talostpl/releases/download/v1.0.0/talostpl-darwin-arm64"
+  sha256 "580d4dd9e7f8e8182f265ea946002c22ed653222ab617e374ba48b1b28d08466"
 
   def install
-    if Hardware::CPU.arm?
-      url = "https://github.com/vasyakrg/talostpl/releases/download/v1.0.0/talostpl-darwin-arm64"
-      sha256 = "580d4dd9e7f8e8182f265ea946002c22ed653222ab617e374ba48b1b28d08466"
-      bin_name = "talostpl-darwin-arm64"
-    else
-      url = "https://github.com/vasyakrg/talostpl/releases/download/v1.0.0/talostpl-darwin-amd64"
-      sha256 = "9843d546bd541b9bf58e2e1c3c85aa8ff0b2f3705630b84b9123be55a99d5202"
-      bin_name = "talostpl-darwin-amd64"
-    end
-    system "curl", "-L", "-o", bin_name, url
-    odie "SHA256 mismatch" unless Digest::SHA256.file(bin_name).hexdigest == sha256
-    bin.install bin_name => "talostpl"
+    bin.install "talostpl-darwin-arm64" => "talostpl"
   end
 
   test do
