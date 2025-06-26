5d9a8a157f1fa5c59ac217e879cae6b15aabc5ed
Vassiliy Yegorov <va@egorovanet.ru>
2025-06-26 09:18:21 +0700

    Упрощена логика установки в формуле Talostpl, добавлены переменные для URL и sha256 в зависимости от архитектуры.

--- Formula/talostpl.rb
+++ Formula/talostpl.rb
@@ -1,20 +1,21 @@
 class Talostpl < Formula
   desc "Interactive and non-interactive Talos K8s config generator"
   homepage "https://github.com/vasyakrg/talostpl"
-
-  on_arm do
-    url "https://github.com/vasyakrg/talostpl/releases/download/v1.0.0/talostpl-darwin-arm64"
-    sha256 "580d4dd9e7f8e8182f265ea946002c22ed653222ab617e374ba48b1b28d08466"
-  end
-
-  on_intel do
-    url "https://github.com/vasyakrg/talostpl/releases/download/v1.0.0/talostpl-darwin-amd64"
-    sha256 "9843d546bd541b9bf58e2e1c3c85aa8ff0b2f3705630b84b9123be55a99d5202"
-  end
+  version "1.0.0"
 
   def install
-    arch = Hardware::CPU.arm? ? "arm64" : "amd64"
-    bin.install "talostpl-darwin-#{arch}" => "talostpl"
+    if Hardware::CPU.arm?
+      url = "https://github.com/vasyakrg/talostpl/releases/download/v1.0.0/talostpl-darwin-arm64"
+      sha256 = "580d4dd9e7f8e8182f265ea946002c22ed653222ab617e374ba48b1b28d08466"
+      bin_name = "talostpl-darwin-arm64"
+    else
+      url = "https://github.com/vasyakrg/talostpl/releases/download/v1.0.0/talostpl-darwin-amd64"
+      sha256 = "9843d546bd541b9bf58e2e1c3c85aa8ff0b2f3705630b84b9123be55a99d5202"
+      bin_name = "talostpl-darwin-amd64"
+    end
+    system "curl", "-L", "-o", bin_name, url
+    odie "SHA256 mismatch" unless Digest::SHA256.file(bin_name).hexdigest == sha256
+    bin.install bin_name => "talostpl"
   end
 
   test do
